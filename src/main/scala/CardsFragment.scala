2432f3f563b7498a7d5c0d347fde556bfe2bad53
Alexandru Scvortov <scvalex@gmail.com>
2012-10-06 12:17:58 +0100

    algebraic suites

--- src/main/scala/CardsFragment.scala
+++ src/main/scala/CardsFragment.scala
@@ -5,10 +5,26 @@ import _root_.android.os.Bundle
 import _root_.android.view.{LayoutInflater, View, ViewGroup}
 import _root_.android.widget.{AdapterView, ArrayAdapter, GridView}
 
+sealed abstract class Suite
+case object Clubs extends Suite {
+  override def toString = "\u2663"
+}
+case object Spades extends Suite {
+  override def toString = "\u2660"
+}
+case object Hearts extends Suite {
+  override def toString = "\u2665"
+}
+case object Diamonds extends Suite {
+  override def toString = "\u2666"
+}
+
 class CardsFragment extends Fragment
   with ToastableFragment
   with AdapterViewOnItemClickListener
 {
+  val suites = List(Clubs, Spades, Hearts, Diamonds)
+
   def handleCardClicked(card : String) : Unit = {
     toast("%s clicked".format(card))
   }
@@ -18,15 +34,15 @@ class CardsFragment extends Fragment
                             savedInstanceState : Bundle) : View =
   {
     val cards = new java.util.ArrayList[String](52)
-    for (suite <- List("C", "S", "H", "D")) {
+    for (suite <- suites) {
       cards.add("A" + suite)
     }
     for (num <- 2 to 10;
-         suite <- List("C", "S", "H", "D")) {
+         suite <- suites) {
       cards.add(num.toString + suite)
     }
     for (num <- List("J", "Q", "K");
-         suite <- List("C", "S", "H", "D")) {
+         suite <- suites) {
       cards.add(num + suite)
     }
     val adapter = new ArrayAdapter[String](getActivity, R.layout.grid_cell, cards)
